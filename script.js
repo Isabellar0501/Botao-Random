33c3160e09e09f8c5a44f8cf324103c0d848fdb4
Isabella Rodrigues <bebellabr@gmail.com>
2023-09-28 15:30:25 -0300

    final commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('no');
+let height = window.innerHeight - 50;
+let width = window.innerWidth - 50;
+button.addEventListener('mouseover', function() {
+    button.style.position = "absolute"
+    button.style.top = Math.random() * height + "px";
+    button.style.left = Math.random() * width + "px";
+})
+
+function sim() {
+    alert("FINALMENTE! TE AMO S2");
+    }
